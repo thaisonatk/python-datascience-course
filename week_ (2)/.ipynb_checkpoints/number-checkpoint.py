d98c915db158a9b78b7979acb0f0c5a129356386
Phi Nguyễn <phi.nguyen.uet@gmail.com>
2023-04-21 15:21:35 +0700

    week 2

---
+++ week_ (2)/.ipynb_checkpoints/number-checkpoint.py
@@ -0,0 +1,9 @@
+class Number:
+    def __init__(self, number):
+        self.number = number
+        
+#     def __str__(self):
+#         return str(self.number)
+    
+    def add_one(self):
+        self_number = self.number + 1
